2a760b1a0e05095499a95894d8d85222240d4834
blackwell <blackwellsmithiv@gmail.com>
2019-02-04 17:54:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,5 +39,14 @@ class Song
   def self.genres
     @@genres.uniq
   end 
-
+  def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_count[artist]
+        @@artist_count[artist] += 1 
+      else 
+        @@artist_count[artist] = 1
+      end
+    end
+    @@artist_count
+  end
 end
